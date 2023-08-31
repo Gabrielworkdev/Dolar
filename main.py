db0d8eaf7faab427f7fada29c592d389bc366a7a
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-08-31 08:45:48 -0300

    new comimt

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#condicao para envio de email
+#cotacao do dolar abaixo de 5.20
+
+
 import requests
 import smtplib
 import email.message
@@ -16,21 +20,25 @@ print(cotacao)
 #enviar um aviso por email
 
 # ---- funcao
-def enviar_email():
-        corpo_email = """
-        <p> Parágrafo1</p>
+# variavel dentro do texto coloca-se f antes do texto e '{}'  ex..{cotacao}  -
+
+
+# para a funcao def funcionar, é necessário para o parâmetro para que ela puxe a informação
+def enviar_email(cotacao):
+        corpo_email = f"""
+        <p> Dolar está abaixo de 5.20. Cotação atual: R${cotacao}</p>
         <p> Parágrafo2</p>
         """
 
         msg = email.message.Message()
-        msg['subject'] = "Assunto"
-        msg['From']  = 'remetente'
-        msg['To'] = 'destinatario'
-        password = 'senha'
+        msg['subject'] = "Dólar está hoje abaixo de 5.20"
+        msg['From']  = 'impressora@cge.rj.gov.br'
+        msg['To'] = 'grodrigues@central.rj.gov.br'
+        password = 'cge@123'
         msg.add_header('content-type', 'text/html')
         msg.set_payload(corpo_email)
 
-        s = smtplib.SMTP('smtp.gmail.com:587')
+        s = smtplib.SMTP('smtps2.webmail.gov.br: 465')
         s.starttls()
         #LOGIN CREDENTIALS FOR SENDING THE MAIL
         s.login(msg['From'], password)
@@ -38,8 +46,12 @@ def enviar_email():
         print('Email enviado')
 
 #codigo padrao 
-
 #coment - requisicao json é um dicionario method a cima 
 
 
-#deploy - heroku
+#logica de condicao 
+if  cotacao < 5.20:
+        enviar_email(cotacao)
+
+#como colocar o codigo no servidor online
+# deploy - heroku
